c9ce6635f224826b70b40257327c264605146f67
Ricky <riggyatwork@gmail.com>
2024-09-10 14:58:47 +0800

    Added two lines to documents

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -5,10 +5,12 @@ public class mainApp1 {
     public static void main(String[] args) {
         int b = 2;
         int n = 11;
+        int c = 15;
         System.out.println("Program... Starting");
         System.out.println("1 + 1 =" + 2 );
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
+        System.out.println(c+"! = " + myLibrary.factorial(c));
         System.out.println("Program ..Ended ...");
     }
 }
