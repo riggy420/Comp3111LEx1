d59d8a6be50f3a1093c41e3b2c1ddb06218da499
Ricky <riggyatwork@gmail.com>
2024-09-10 14:59:47 +0800

    Added one more lines to it

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -7,6 +7,7 @@ public class mainApp1 {
         int n = 11;
         int c = 15;
         System.out.println("Program... Starting");
+        System.out.println("Good Morning!");
         System.out.println("1 + 1 =" + 2 );
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
